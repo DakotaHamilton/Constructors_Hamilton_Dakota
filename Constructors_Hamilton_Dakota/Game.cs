a856e2669f7ed39f41552f3ad99ec54e27b8e5af
DakotaHamilton <113059394+DakotaHamilton@users.noreply.github.com>
2023-01-19 08:31:03 -0600

    Add project files.

---
+++ Constructors_Hamilton_Dakota/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors_Hamilton_Dakota
+{
+    class Game
+    {
+        public string title, genre; // String Fields
+
+        public Game(string titleparam = "Unknown Title", string genreParam = "Casual") // Class Constructor
+        {
+            title = titleparam; // String Param 1
+            genre = genreParam; // String Param 2
+        }
+    }
+}
---
+++ Constructors_Hamilton_Dakota/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Constructors_Hamilton_Dakota
+{
+    class Program
+    {
+        static void Main(string[] args) // Main Method
+        {
+            Game myGame = new Game("Fortnite", "T for Teen"); // Class Variables
+
+            Console.WriteLine($"My favorite game is {myGame.title} and it is rated {myGame.genre}!"); // Writes to the Console the Message with the Variable Values
+
+            Game myOtherGame = new Game(); // Default Class 
+
+            Console.WriteLine($"My favorite game is {myOtherGame.title} and it is rated {myOtherGame.genre}!"); // Writes to the Console the Default Class
+        }
+    }
+}
